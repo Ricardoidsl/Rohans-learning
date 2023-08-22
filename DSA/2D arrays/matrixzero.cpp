c357ad47a74e02a942c5a37376b728913506502e
Rohan Singh <99131732+Ricardoidsl@users.noreply.github.com>
2023-08-22 15:08:41 +0530

    Add files via upload

---
+++ DSA/2D arrays/matrixzero.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+vector <vector<int>> zero(vector <vector<int>> matrix, int n, int m)
+{
+    int col[m] = {0};
+    int row[n] = {0};
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            if(matrix[i][j] == {0})
+            row[i] = 1;
+            col[j] = 1;
+        }
+    }
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            if(row[i] == 1 || col[j] == 1)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
